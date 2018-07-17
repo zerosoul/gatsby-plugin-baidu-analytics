bccd685d9f133cb61176adf0d6d09a413a4e4c9b
Tristan <yanggc888@163.com>
2018-07-17 23:18:30 +0800

    fix bug

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -3,8 +3,8 @@ exports.onRenderBody = (
   { setHeadComponents, setPostBodyComponents },
   { siteid, head }
 ) => {
-  if (process.env.NODE_ENV == `production`) {
-    const BAIDU_ANALYTICS_SCRIPT = () => (
+  if (process.env.NODE_ENV === `production`) {
+    const BAIDU_ANALYTICS_SCRIPT = (
       <script
         key={`gatsby-plugin-baidu-analytics-script`}
         dangerouslySetInnerHTML={{
@@ -12,7 +12,7 @@ exports.onRenderBody = (
                   var _hmt = _hmt || [];
                   (function() {
                       var hm = document.createElement("script");
-                      hm.src = "https://hm.baidu.com/hm.js?${siteid}";
+                      hm.src = "//hm.baidu.com/hm.js?${siteid}";
                       var s = document.getElementsByTagName("script")[0]; 
                       s.parentNode.insertBefore(hm, s);
                   })();
